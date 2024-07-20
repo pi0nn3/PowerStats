3186d4a782d6c08373de18ce9838bae32c73f0fe
pi0nn3 <cedonak269@cnurbano.com>
2024-07-20 21:06:01 +0300

    Proje dosyası ekle.

---
+++ PowerStats/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace PowerStats
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 254);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Batarya Bilgileri";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(59, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(59, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 29);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(59, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 29);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(59, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 29);
+            this.label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(59, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 29);
+            this.label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(46, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 29);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(46, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 29);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(59, 394);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 29);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(46, 165);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 29);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(46, 210);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(95, 29);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "label10";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Location = new System.Drawing.Point(12, 276);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 254);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sistem Bilgileri";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 542);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PowerStats";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ PowerStats/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+
+namespace PowerStats
+{
+    public partial class Form1 : Form
+    {
+        private Timer zamanlayici1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            zamanlayici1 = new Timer
+            {
+                Interval = 5000 // 5 saniye
+            };
+            zamanlayici1.Tick += zamanlayici1_Tick;
+            zamanlayici1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PilDurumunuGuncelle();
+            SistemBilgileriniGuncelle();
+        }
+
+        private void zamanlayici1_Tick(object sender, EventArgs e)
+        {
+            PilDurumunuGuncelle();
+            SistemBilgileriniGuncelle();
+        }
+
+        private void PilDurumunuGuncelle()
+        {
+            try
+            {
+                // Pil bilgilerini almak için WMI sorgusu
+                string pilSorgusu = "SELECT * FROM Win32_Battery";
+                using (var pilArayici = new ManagementObjectSearcher("root\\CIMv2", pilSorgusu))
+                {
+                    var pilKoleksiyonu = pilArayici.Get();
+
+                    if (pilKoleksiyonu.Count == 0)
+                    {
+                        MessageBox.Show("Pil bilgileri alınamadı. Bilgisayarınızda WMI pil bilgileri bulunamadı.");
+                        return;
+                    }
+
+                    foreach (ManagementObject nesne in pilKoleksiyonu)
+                    {
+                        // Pil Yüzdesi
+                        label1.Text = nesne["EstimatedChargeRemaining"] != null
+                            ? $"Pil Yüzdesi: {nesne["EstimatedChargeRemaining"]}%"
+                            : "Pil yüzdesi bilgisi mevcut değil";
+
+                        // Şarj Durumu
+                        var sarjDurumu = nesne["BatteryStatus"] != null
+                            ? Convert.ToInt32(nesne["BatteryStatus"])
+                            : -1;
+                        label2.Text = sarjDurumu == 2
+                            ? "Şarj Oluyor"
+                            : sarjDurumu == 1
+                            ? "Şarj Edilmiyor"
+                            : "Şarj durumu bilgisi mevcut değil";
+
+                        // Kapasite
+                        var tamSarjKapasitesi = nesne["FullChargeCapacity"] != null
+                            ? nesne["FullChargeCapacity"].ToString()
+                            : "Bilgi mevcut değil";
+                        var tasarimKapasitesi = nesne["DesignCapacity"] != null
+                            ? nesne["DesignCapacity"].ToString()
+                            : "Bilgi mevcut değil";
+                        label3.Text = $"Kapasite: {tamSarjKapasitesi} mAh / {tasarimKapasitesi} mAh";
+
+                        // Kimya Bilgisi
+                        var kimyaKodu = nesne["Chemistry"] != null
+                            ? Convert.ToInt32(nesne["Chemistry"])
+                            : -1;
+                        label4.Text = $"Pil Kimyası: {KimyaAciklamasiAl(kimyaKodu)}";
+
+                        // Tahmini Çalışma Süresi
+                        var tahminiCalismaSuresi = nesne["EstimatedRunTime"] != null
+                            ? Convert.ToInt64(nesne["EstimatedRunTime"])
+                            : 0;
+                        label5.Text = $"Tahmini Çalışma Süresi: {CalismaSuresiniFormatla(tahminiCalismaSuresi)}";
+                    }
+                }
+            }
+            catch (ManagementException mex)
+            {
+                MessageBox.Show($"WMI hatası oluştu: {mex.Message}\n{mex.StackTrace}");
+            }
+            catch (UnauthorizedAccessException uaex)
+            {
+                MessageBox.Show($"Erişim hatası oluştu: {uaex.Message}\n{uaex.StackTrace}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}\n{ex.StackTrace}");
+            }
+        }
+
+        private void SistemBilgileriniGuncelle()
+        {
+            try
+            {
+                // Sistem adını almak için WMI sorgusu
+                string sistemSorgusu = "SELECT * FROM Win32_ComputerSystem";
+                using (var sistemArayici = new ManagementObjectSearcher("root\\CIMv2", sistemSorgusu))
+                {
+                    var sistemKoleksiyonu = sistemArayici.Get();
+
+                    if (sistemKoleksiyonu.Count > 0)
+                    {
+                        foreach (ManagementObject nesne in sistemKoleksiyonu)
+                        {
+                            var sistemAdi = nesne["Name"] != null
+                                ? nesne["Name"].ToString()
+                                : "Bilinmiyor";
+                            label6.Text = $"Sistem Adı: {sistemAdi}";
+
+                            var ramBilgisi = nesne["TotalPhysicalMemory"] != null
+                                ? $"{Convert.ToInt64(nesne["TotalPhysicalMemory"]) / (1024 * 1024)} MB"
+                                : "Bilinmiyor";
+                            label8.Text = $"RAM Bilgisi: {ramBilgisi}";
+
+                            var sistemTuru = nesne["SystemType"] != null
+                                ? nesne["SystemType"].ToString()
+                                : "Bilinmiyor";
+                            label9.Text = $"Sistem Türü: {sistemTuru}";
+                        }
+                    }
+                }
+
+                // İşlemci adını almak için WMI sorgusu
+                string islemciSorgusu = "SELECT * FROM Win32_Processor";
+                using (var islemciArayici = new ManagementObjectSearcher("root\\CIMv2", islemciSorgusu))
+                {
+                    var islemciKoleksiyonu = islemciArayici.Get();
+
+                    if (islemciKoleksiyonu.Count > 0)
+                    {
+                        foreach (ManagementObject nesne in islemciKoleksiyonu)
+                        {
+                            var islemciAdi = nesne["Name"] != null
+                                ? nesne["Name"].ToString()
+                                : "Bilinmiyor";
+                            label7.Text = $"İşlemci Adı: {islemciAdi}";
+                        }
+                    }
+                }
+
+                // Ürün kimliğini almak için WMI sorgusu
+                string urunKimiğiSorgusu = "SELECT * FROM Win32_OperatingSystem";
+                using (var urunKimiğiArayici = new ManagementObjectSearcher("root\\CIMv2", urunKimiğiSorgusu))
+                {
+                    var urunKimiğiKoleksiyonu = urunKimiğiArayici.Get();
+
+                    if (urunKimiğiKoleksiyonu.Count > 0)
+                    {
+                        foreach (ManagementObject nesne in urunKimiğiKoleksiyonu)
+                        {
+                            var urunKimiği = nesne["SerialNumber"] != null
+                                ? nesne["SerialNumber"].ToString()
+                                : "Bilinmiyor";
+                            label10.Text = $"Ürün Kimliği: {urunKimiği}";
+                        }
+                    }
+                }
+            }
+            catch (ManagementException mex)
+            {
+                MessageBox.Show($"WMI hatası oluştu: {mex.Message}\n{mex.StackTrace}");
+            }
+            catch (UnauthorizedAccessException uaex)
+            {
+                MessageBox.Show($"Erişim hatası oluştu: {uaex.Message}\n{uaex.StackTrace}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}\n{ex.StackTrace}");
+            }
+        }
+
+        private string CalismaSuresiniFormatla(long saniye)
+        {
+            long saat = saniye / 3600;
+            long dakika = (saniye % 3600) / 60;
+            long kalanSaniye = saniye % 60;
+
+            return $"{saat} saat {dakika} dakika {kalanSaniye} saniye";
+        }
+
+        private string KimyaAciklamasiAl(int kimyaKodu)
+        {
+            switch (kimyaKodu)
+            {
+                case 1:
+                    return "Lityum İyon";
+                case 2:
+                    return "Lityum Polimer";
+                case 3:
+                    return "Nikkel Kobalt Alüminyum Oksit (NCA)";
+                case 4:
+                    return "Nikkel Mangan Kobalt (NMC)";
+                default:
+                    return "Bilinmiyor";
+            }
+        }
+    }
+}
